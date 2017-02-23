ee5f95764734c96be2d53367570e9d197a65a543
silen <silendir@126.com>
2017-02-23 18:15:11 +0800

    block回调

---
+++ LJAreaPickerView/PickerViewController.h
@@ -0,0 +1,26 @@
+//
+//  LJArea.h
+//  CBProjects
+//
+//  Created by silen on 2017/2/23.
+//  Copyright © 2017年 CB. All rights reserved.
+//
+
+//宏
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+typedef void (^ReturnValueBlock) (id returnValue);
+#import <UIKit/UIKit.h>
+
+@interface PickerViewController : UIViewController
+    @property (weak, nonatomic) IBOutlet UILabel *label;
+    @property (nonatomic,strong)UIColor* closeButtonTitleColor;
+    @property (nonatomic,strong)UIColor* pickerViewBackgroundColor;
+    @property (nonatomic,strong)UIView* showView;
+- (void)initWithPickerColor:(UIColor*)pickerColor
+               CloseButtonColor:(UIColor*)closeButtonColor
+                       showView:(UIView*)vie
+                        inBlock:(ReturnValueBlock)block;
+- (void)didClickArea;
+    @property (nonatomic,copy)ReturnValueBlock block;
+@end
--- LJAreaPickerView/ViewController.m
+++ LJAreaPickerView/ViewController.m
@@ -5,17 +5,18 @@
 //  Created by silen on 2017/2/23.
 //  Copyright © 2017年 silen. All rights reserved.
 //
-
+#import "PickerViewController.h"
 #import "ViewController.h"
 
 @interface ViewController ()
-
+    @property (nonatomic,strong) PickerViewController* picker;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _picker = [PickerViewController new];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +24,13 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)dddd:(id)sender {
+    
+    [_picker initWithPickerColor:[UIColor whiteColor] CloseButtonColor:[UIColor blueColor] showView:self.view inBlock:^(id returnValue) {
+        self.label.text = returnValue;
+    }];
+     
 }
 
 
